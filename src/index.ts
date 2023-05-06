38308f1d6b7dcf773d7b2d4fa636c7f7576c058d
aswinr <aswinr019@gmail.com>
2023-05-06 19:31:21 +0530

    minor changes

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,19 @@ app.listen(PORT,() => {
   console.log(`listening on port http://localhost:${PORT}`);
 });
 
-app.get('/',(req: Request ,res: Response) => {
-  res.send("Hello World!");
+app.get('/',(req: Request , res: Response) => {
+  res.send("UTMS is live!");
+});
+
+
+app.post('/signup',(req: Request , res: Response) => {
+  const { fName ,lName, email , phoneNumber , password } = req.body;
+});
+
+app.post('/signin',(req: Request , res: Response) => {
+  const { email , password } = req.body;
+});
+
+app.post('/destinations', (req: Request , res: Response) => {
+  
 });
