2a2edc6a9290816d1a794969db98d161a8db7763
aswinr <aswinr019@gmail.com>
2023-05-11 15:11:55 +0530

    added database connection , server start functionality , and route for health cehck

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ mongoose
     .then(() => {
         Logging.info('Connected to mongoDB.');
     })
-    .catch((error:any) => {
+    .catch((error: any) => {
         Logging.error('Unable to connect:');
         Logging.error(error);
     });
@@ -34,7 +34,41 @@ const StartServer = () => {
 
         next();
     });
+
+    router.use(express.urlencoded({ extended: true }));
+    router.use(express.json());
+
+    //Rules for api
+
+    router.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header(
+            'Access-Control-Alloe-Headers',
+            'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+        );
+
+        if (req.method == 'OPTIONS') {
+            res.header(
+                'Access-Control-Allow-Methods',
+                'PUT, POST, PATCH, DELETE, GET'
+            );
+            return res.status(200).json({});
+        }
+        next();
+    });
+
+    //Routes
+
+    //Healthcheck
+    router.get('/ping', (req, res, next) => {
+        res.status(200).json({ message: 'pong' });
+    });
+
+    //Error handling
+    router.use((req, res, next) => {
+        const error = new Error('not found');
+        Logging.error(error);
+
+        return res.status(404).json({ message: error.message });
+    });
 };
-
-
-
