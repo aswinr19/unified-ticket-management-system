37af449eafb3f27dcea436215cce80a4d98568aa
aswinr <aswinr019@gmail.com>
2023-05-13 19:46:35 +0530

    added base for creating controllers

---
+++ src/controllers/User.ts
@@ -0,0 +1,32 @@
+import { NextFunction , Request , Response } from 'express';
+import mongoose from 'mongoose';
+import User from '../models/User';
+
+const signup = (req: Request , res: Response , next: nextFunction) => {
+
+  const { name , email , phone , password } = req.body;
+
+  const user = new User({
+    _id: new mongoose.Types.ObjectId(),
+    name,
+    email,
+    phone,
+    password
+  });
+
+  return user
+  .save()
+  .then((user) => res.status(201).json({ user }))
+  .catch((error) => res.status(500).json({ error }));
+};
+
+const signin = (req: Request , res: Response , next: nextFunction) => {
+
+  const { email , password } = req.body;
+
+  User.find().then().catch();
+};
+
+
+export default { signup , signin };
+
