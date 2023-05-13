e0e01d5cf5bc474f3e7d28182ed31f50f67d6aff
aswinr <aswinr019@gmail.com>
2023-05-13 16:37:48 +0530

    added user schema

---
+++ src/models/User.ts
@@ -0,0 +1,30 @@
+import mongoose , { Document , Schema } from 'mongoose';
+
+
+export interface User {
+  firstname: string,
+  lastname: string,
+  username: string,
+  email: string,
+  phone: number,
+  password: string,
+}
+
+
+export interface UserModel extends User , Document {}
+
+  const UserSchema: Schema = new Schema(
+    {
+      firstname: { type: String , required: true },
+      lastname: { type: String , required: true },
+      username: { type: String , required: true },
+      email: { type: String , required: true },
+      phone: {type : Number , required: true },
+      password: { type: String , required: true },
+    },
+    {
+      timestamps: true
+    }
+  );
+
+export default mongoose.model<UserModel>('User',UserSchema);
