ec03a2adb9d82d918159f732b47e0bfa7f743f58
arjunr2226 <2226@tkmce.ac.in>
2023-05-20 19:19:01 +0530

    hidden unwanted fields in findAllUsers

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,11 +4,11 @@ dotenv.config();
 
 const MONGO_USERNAME = process.env.MONGO_USERNAME || '';
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || '';
-const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@utms.wmfqznf.mongodb.net/`;
+const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@utms.hwsng0b.mongodb.net/`;
 
 const SERVER_PORT = process.env.SERVER_PORT
     ? Number(process.env.SERVER_PORT)
-    : 3000;
+    : 3001;
 
 export const config = {
     mongo: {
