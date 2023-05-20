8bc192641650ae06bfcc5f73ce02d419e2bee2c8
aswinr <aswinr019@gmail.com>
2023-05-20 13:30:23 +0530

    added userNameGenerator function and fixed createUser

--- src/controllers/User.ts
+++ src/controllers/User.ts
@@ -1,13 +1,50 @@
 import { NextFunction, Request, Response } from 'express';
 import mongoose from 'mongoose';
+import { generateFromEmail, generateUsername } from "unique-username-generator";
 import User from '../models/User';
 
-const createUser = (req: Request, res: Response, next: NextFunction) => {};
-const readUser = (req: Request, res: Response, next: NextFunction) => {};
-const readAllUser = (req: Request, res: Response, next: NextFunction) => {};
+const generateUserName = (email: string): string => {
+   return generateFromEmail(email,4);
+};
+
+
+const createUser = async (req: Request, res: Response, next: NextFunction) => {
+
+  const { firstName , lastName , email , phone , password } = req.body;
+  const userName = generateUserName(email);
+
+  try{
+    const user = await User.create({
+      firstName ,
+      lastName ,
+      userName,
+      email , 
+      phone , 
+      password
+    });
+
+    res.status(201).json({ user });
+  }
+  catch(error){
+    res.status(400).json({ error });
+  }
+};
+
+const readUser = async (req: Request, res: Response, next: NextFunction) => {
+
+};
+
+const readAllUser = (req: Request, res: Response, next: NextFunction) => {
+
+};
+
 const updateUser = (req: Request, res: Response, next: NextFunction) => {};
 const deleteUser = (req: Request, res: Response, next: NextFunction) => {};
-const login = (req: Request, res: Response, next: NextFunction) => {};
+
+const login = (req: Request, res: Response, next: NextFunction) => {
+
+  const { email , password } = req.body;
+};
 
 export default {
     createUser,
--- src/models/User.ts
+++ src/models/User.ts
@@ -15,9 +15,9 @@ export interface UserModel extends User , Document {}
 
   const UserSchema: Schema = new Schema(
     {
-      firstname: { type: String , required: true },
-      lastname: { type: String , required: true },
-      username: { type: String , required: true },
+      firstName: { type: String , required: true },
+      lastName: { type: String , required: true },
+      userName: { type: String , required: true },
       email: { type: String , required: true },
       phone: {type : Number , required: true },
       password: { type: String , required: true },
